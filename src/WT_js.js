72405f9fe259f00e1a2a4bb367791d82c6bfbc15
DHAiRYA2048 <dhairyashah2801@gmail.com>
2022-11-22 02:29:14 +0530

    Updated button

--- src/WT_js.js
+++ src/WT_js.js
@@ -11,5 +11,37 @@ function update() {
 	
 }
 
-//Split([".container", ".iframe-container"]);
-//Split(["#htmlCode", "#cssCode", "#javascriptCode"]);
+function download(){
+    var text1 = document.getElementById("htmlCode").value;
+    var blob = new Blob([text1], { type: "text/plain"});
+    var anchor = document.createElement("a");
+    anchor.download = "index.html";
+	anchor.href = window.URL.createObjectURL(blob);
+    anchor.target ="_blank";
+    anchor.style.display = "none"; 
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+
+	var text2 = document.getElementById("cssCode").value;
+	var blob = new Blob([text2], { type: "text/plain"});
+    var anchor = document.createElement("a");
+    anchor.download = "index.css";
+	anchor.href = window.URL.createObjectURL(blob);
+    anchor.target ="_blank";
+    anchor.style.display = "none";
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+
+	var text3 = document.getElementById("javascriptCode").value;
+	var blob = new Blob([text3], { type: "text/plain"});
+    var anchor = document.createElement("a");
+    anchor.download = "index.js";
+    anchor.href = window.URL.createObjectURL(blob);
+    anchor.target ="_blank";
+    anchor.style.display = "none"; 
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+ }
