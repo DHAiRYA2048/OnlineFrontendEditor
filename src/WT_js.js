c8ed5a5437113d8e84cd873095d0b51a8ee413b9
DHAiRYA2048 <dhairyashah2801@gmail.com>
2022-11-22 00:15:04 +0530

    Updated files

--- src/WT_js.js
+++ src/WT_js.js
@@ -3,7 +3,7 @@ function update() {
 	let htmlCode=document.getElementById("htmlCode").value;
 	let cssCode=document.getElementById("cssCode").value;
 	let javascriptCode=document.getElementById("javascriptCode").value;
-	let text=htmlCode+"<style>"+cssCode+"</style>"+"<scri"+"pt>"+javascriptCode+"</scri"+"pt>";
+	let text=htmlCode+"<style>"+cssCode+"</style>"+"<script>"+javascriptCode+"</script>";
 	let iframe=document.getElementById('viewer').contentWindow.document;
 	iframe.open();
 	iframe.write(text);
@@ -11,5 +11,5 @@ function update() {
 	
 }
 
-Split([".container", ".iframe-container"]);
+//Split([".container", ".iframe-container"]);
 //Split(["#htmlCode", "#cssCode", "#javascriptCode"]);
